216e8c199e5cd5c62d62b0756857b08bdbf145d4
Emac <shen.bin.1983@gmail.com>
2017-05-28 23:23:48 +0800

    phase III

--- src/main/java/cn/emac/demo/spring5/repositories/EmployeeRepository.java
+++ src/main/java/cn/emac/demo/spring5/repositories/EmployeeRepository.java
@@ -1,8 +1,8 @@
 package cn.emac.demo.spring5.repositories;
 
 import cn.emac.demo.spring5.domain.Employee;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
 }
